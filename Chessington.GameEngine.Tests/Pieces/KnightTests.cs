83018c7f14efcd8450895f1ae4bb4707adb9f577
espe-on <eljoe0@googlemail.com>
2020-02-14 16:46:30 +0000

    initialised piece tests for Bishop, Knight, Queen, and Rook

--- Chessington.GameEngine.Tests/Pieces/KnightTests.cs
+++ Chessington.GameEngine.Tests/Pieces/KnightTests.cs
@@ -1,9 +1,22 @@
-﻿﻿using NUnit.Framework;
+﻿﻿using System.Collections.Generic;
+ using Chessington.GameEngine.Pieces;
+ using NUnit.Framework;
 
 namespace Chessington.GameEngine.Tests.Pieces
 {
     [TestFixture]
     public class KnightTests
     {
+        private Board _board;
+        private Piece _king;
+        private List<Square> _moveList; 
+
+        [SetUp]
+        public void SetUp()
+        {
+            _board = new Board();
+            _king = new King(Player.Black);
+            _moveList = new List<Square>();
+        }
     }
 }

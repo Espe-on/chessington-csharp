b325dfc1b28027438ad9eb4e99ed993ffe1e00cd
espe-on <eljoe0@googlemail.com>
2020-02-14 16:26:30 +0000

    movelist now private. same colour piece taking test now there

--- Chessington.GameEngine.Tests/Pieces/KingTests.cs
+++ Chessington.GameEngine.Tests/Pieces/KingTests.cs
@@ -10,46 +10,54 @@ namespace Chessington.GameEngine.Tests.Pieces
     {
         private Board _board;
         private Piece _king;
+        private List<Square> _moveList; 
 
         [SetUp]
         public void SetUp()
         {
             _board = new Board();
             _king = new King(Player.Black);
+            _moveList = new List<Square>();
         }
+        
         [Test]
         public void King_CanMoveOneSquareInAnyDirection()
         {
             _board.AddPiece(Square.At(3,3), _king);
-            var moves = _king.GetAvailableMoves(_board);
-            List<Square> moveList = new List<Square>();
-            moveList.Add(Square.At(4,2 ));
-            moveList.Add(Square.At(4,3 ));
-            moveList.Add(Square.At(4,4 ));
-            moveList.Add(Square.At(3,2 ));
-            moveList.Add(Square.At(3,4 ));
-            moveList.Add(Square.At(2,2 ));
-            moveList.Add(Square.At(2,3 ));
-            moveList.Add(Square.At(2,4 ));
-            moves.Should().BeEquivalentTo(moveList);
+            IEnumerable<Square> moves = _king.GetAvailableMoves(_board);
+            _moveList.Add(Square.At(4,2 ));
+            _moveList.Add(Square.At(4,3 ));
+            _moveList.Add(Square.At(4,4 ));
+            _moveList.Add(Square.At(3,2 ));
+            _moveList.Add(Square.At(3,4 ));
+            _moveList.Add(Square.At(2,2 ));
+            _moveList.Add(Square.At(2,3 ));
+            _moveList.Add(Square.At(2,4 ));
+            
+            moves.Should().BeEquivalentTo(_moveList);
         }
+        
         [Test]
         public void King_CantMoveOffBoard()
         {
             _board.AddPiece(Square.At(7,7), _king);
-            var moves = _king.GetAvailableMoves(_board);
+            IEnumerable<Square> moves = _king.GetAvailableMoves(_board);
+            _moveList.Add(Square.At(7,6 ));
+            _moveList.Add(Square.At(6,6 ));
+            _moveList.Add(Square.At(6,7 ));
 
-            moves.Should().NotContain(Square.At(7, 8));
-            moves.Should().NotContain(Square.At(8, 7));
-            moves.Should().NotContain(Square.At(8, 8));
+            moves.Should().BeEquivalentTo(_moveList);
         }
-
-        /*[Test]
+        
+        [Test]
         public void King_CantTakePieceOfSameColour()
         {
             Piece target = new Pawn(Player.Black);
             _board.AddPiece(Square.At(5,5 ), _king);
             _board.AddPiece(Square.At(5,6 ), target);
-        }*/
+            IEnumerable<Square> moves = _king.GetAvailableMoves(_board);
+            
+            moves.Should().NotContain(Square.At(5, 6));
+        }
     }
 }

4318746f79894a6e41172605686ec905b6a1728f
espe-on <eljoe0@googlemail.com>
2020-02-14 16:11:13 +0000

    King Move test added and working

--- Chessington.GameEngine.Tests/Pieces/KingTests.cs
+++ Chessington.GameEngine.Tests/Pieces/KingTests.cs
@@ -1,4 +1,5 @@
-﻿﻿using Chessington.GameEngine.Pieces;
+﻿﻿using System.Collections.Generic;
+ using Chessington.GameEngine.Pieces;
  using FluentAssertions;
  using NUnit.Framework;
 
@@ -7,16 +8,48 @@ namespace Chessington.GameEngine.Tests.Pieces
     [TestFixture]
     public class KingTests
     {
-        [Test]
-        public void King_CanMoveOneSquareUp()
+        private Board _board;
+        private Piece _king;
+
+        [SetUp]
+        public void SetUp()
         {
-            var board = new Board();
-            var king = new King(Player.Black);
-            board.AddPiece(Square.At(4,5), king);
-
-            var moves = king.GetAvailableMoves(board);
-
-            moves.Should().Contain(Square.At(5, 5));
+            _board = new Board();
+            _king = new King(Player.Black);
         }
+        [Test]
+        public void King_CanMoveOneSquareInAnyDirection()
+        {
+            _board.AddPiece(Square.At(3,3), _king);
+            var moves = _king.GetAvailableMoves(_board);
+            List<Square> moveList = new List<Square>();
+            moveList.Add(Square.At(4,2 ));
+            moveList.Add(Square.At(4,3 ));
+            moveList.Add(Square.At(4,4 ));
+            moveList.Add(Square.At(3,2 ));
+            moveList.Add(Square.At(3,4 ));
+            moveList.Add(Square.At(2,2 ));
+            moveList.Add(Square.At(2,3 ));
+            moveList.Add(Square.At(2,4 ));
+            moves.Should().BeEquivalentTo(moveList);
+        }
+        [Test]
+        public void King_CantMoveOffBoard()
+        {
+            _board.AddPiece(Square.At(7,7), _king);
+            var moves = _king.GetAvailableMoves(_board);
+
+            moves.Should().NotContain(Square.At(7, 8));
+            moves.Should().NotContain(Square.At(8, 7));
+            moves.Should().NotContain(Square.At(8, 8));
+        }
+
+        /*[Test]
+        public void King_CantTakePieceOfSameColour()
+        {
+            Piece target = new Pawn(Player.Black);
+            _board.AddPiece(Square.At(5,5 ), _king);
+            _board.AddPiece(Square.At(5,6 ), target);
+        }*/
     }
 }

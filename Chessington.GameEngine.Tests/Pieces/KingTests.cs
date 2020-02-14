e26bf526400899d3a16220898bf5ef4c3414f721
espe-on <eljoe0@googlemail.com>
2020-02-14 10:25:32 +0000

    added in King_CanMoveUpOneSquare test

--- Chessington.GameEngine.Tests/Pieces/KingTests.cs
+++ Chessington.GameEngine.Tests/Pieces/KingTests.cs
@@ -1,9 +1,22 @@
-﻿﻿using NUnit.Framework;
+﻿﻿using Chessington.GameEngine.Pieces;
+ using FluentAssertions;
+ using NUnit.Framework;
 
 namespace Chessington.GameEngine.Tests.Pieces
 {
     [TestFixture]
     public class KingTests
     {
+        [Test]
+        public void King_CanMoveOneSquareUp()
+        {
+            var board = new Board();
+            var king = new King(Player.Black);
+            board.AddPiece(Square.At(4,5), king);
+
+            var moves = king.GetAvailableMoves(board);
+
+            moves.Should().Contain(Square.At(5, 5));
+        }
     }
 }
